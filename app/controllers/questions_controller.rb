14b20de1e8b13e524d42b0e11d7e904bc249aff2
casey tokarchuk <caseytokarchuk@gmail.com>
2022-08-08 14:51:31 -0400

    working

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,5 +4,15 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
+
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question = 'what time is it now?'
+      @answer = Time.now
+    elsif @question[-1] == '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
